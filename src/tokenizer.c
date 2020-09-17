5cf388315da5c0b18954f3f36291232bc9f4d6c6
h3ck3rman <ngarci2032@gmail.com>
2020-09-16 23:34:32 -0600

    modified and completed all second milestone functions in tokenizer.c + added history.c to file list and modified ui for testing

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -4,36 +4,52 @@
 
 int space_char(char c)
 {
-  if(c < '!')
-    {
-      return 1;
-    }
-  else
-    {
-      return 0;
-    }
+  return c < '!';
 }
 
 int non_space_char(char c)
 {
-  if(c >= '!')
-    {
-      return 1;
-    }
-  else
-    {
-      return 0;
-    }
+  return c >= '!';
 }
 
 char *word_start(char *str)
 {
-  int n = 0;
-  while(space_char(str[0+n]) == 1)
+  for(int i = 0; str[i] != '\0'; i++)
     {
-      n++;
+      if(non_space_char(str[i]))
+	{
+	  return &str[i];
+	}
     }
-  return &str[0+n];
+  return &str[0];
+}
+
+char *word_terminator(char *word)
+{
+  for(int i = 0; word[i] != '\0'; i++)
+    {
+      if(non_space_char(word[i]) && space_char(word[i+1]))
+	{
+	  return &word[i+1];
+	}
+    }
+  return word;
+}
+
+int count_words(char *str)
+{
+  int counter = 0;
+  char *word = word_start(&str[0]);
+  while(*word != '\0')
+    {
+      if(non_space_char(*word))
+	{
+	  counter++;
+	}
+      word = word_terminator(word);
+      word = word_start(word);
+    }
+  return counter;
 }
 
 
--- src/uimain.c
+++ src/uimain.c
@@ -5,21 +5,33 @@ int main(){
   char c;
   char s[50];
   puts("Welcome");
- 
+  
   printf("\nspace_char function\n");
   printf("Enter Char: ");
   scanf("%c", &c);
   printf("%d",space_char(c));
-
+  /*
   printf("\nnon_space_char function\n");
   printf("Enter Char: ");
   scanf(" %c", &c);
   printf("%d",non_space_char(c));
-
+  */
   printf("\nword_start function\n");
   printf("Enter string: ");
   scanf(" %[^\n]",s);
   printf("first non-space char is: %c\n", *word_start(&s[0]));
+
+  char *str = "waiting for the PS5";
+  printf("\nword_terminator\n");
+  printf("Selected string: %s\n", str);
+  printf("First address: %d\n", &str[0]);
+  printf("Pointed address after word_terminator: %d\n", word_terminator(&str[0]));
+  
+  printf("\ncount_words function\n");
+  printf("Enter string: ");
+  scanf(" %[^\n]",s);
+  printf("the number of words is %d\n", count_words(&s[0]));
+  */
   return 0;
 }
   
